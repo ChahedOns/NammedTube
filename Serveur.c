664a8384fee958dcbb5c3b8f71e5d16dfc5d271f
Chahed Ons <58376907+ChahedOns@users.noreply.github.com>
2022-11-10 22:40:25 +0100

    Update Serveur.c

--- Serveur.c
+++ Serveur.c
@@ -8,22 +8,22 @@ void hand_wakeUpClt(int sig){
 }
  
 void hand_endServ(int sig){
-	if (sig == SIGKIL){
+	
 	printf("Fin de tâche! \n");
-	exit();}
+	exit(2);
 }
-
+int f1,f2;
 main(){
 /*Déclarations */
-response rs ; 
-request rq ; 
+struct response rs ; 
+struct request rq ; 
 
 /* Création des tubes nommés */
 
-if ( mkfifo(fifo1,S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH) == -1 ){
+if ( mkfifo(FILE1,0666) == -1 ){
 	perror("Error lors de la création du tube 1 !");
 }
-if ( mkfifo(fifo2,S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH) == -1 ){
+if ( mkfifo(FILE2,0666) == -1 ){
 	perror("Error lors de la création du tube 2 !");
 }
 
@@ -33,27 +33,33 @@ srand(getpid());
 
 /* Ouverture des tubes nommés */
 
-f1 = open(fifo1, O_RDONLY);
-f2 = open(fifo2 , O_WRONLY);
-
-
+f1 = open(FILE1,O_RDONLY);
+f2 = open(FILE2 ,O_WRONLY);
+if ( f1 == -1 || f2 == -1)
+{
+printf (" Ouverture des tubes impossible ");
+exit(-1);
+};
+printf(" valeur de f1 = %d \n valeur de f2 = %d ", f1,f2 );
 /* Installation des Handlers */
 	
 	signal(SIGINT,hand_endServ);
 	signal(SIGUSR1,hand_wakeUpClt);
 while(1){
+	printf("im here!");
 	/* lecture d’une question */
-		 read(f1,rq,1);
+		 read (f1 ,&rq , sizeof ( struct request ));
 	/* construction de la réponse */
-		for (int i=0 ; i<rq.n ; i++){
-			rs.t[i] = rand();
+		 int i;
+		for (i=0 ; i<rq.n ; i++){
+			rs.t[i] = rand()%NMAX;
 		}
 		rs.idc = rq.id;
 		rs.ids = getpid();
 	/* envoi de la réponse */
-		write(f2,rs);
+		write(f2,&rs, sizeof (struct response));
 	/* envoi du signal SIGUSR1 au client concerné */
-		kill(rs.id,SINGUSR1);
+		kill(rs.idc,SIGUSR1);
 }
 close(f1);
 close(f2);
