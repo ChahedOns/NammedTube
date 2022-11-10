ca68642749370868294aba25951bfb33f8e48962
Chahed Ons <58376907+ChahedOns@users.noreply.github.com>
2022-11-10 22:39:36 +0100

    Update ser_cli_fifo.h

--- ser_cli_fifo.h
+++ ser_cli_fifo.h
@@ -3,16 +3,15 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/stat.h>
-
-const int NMAX=10;
+#include<fcntl.h>
 
 // FIFO file path
-const char * fifo1 = "./fifo1";
-const char * fifo2 = "./fifo2";
+//const char * fifo1 = "./fifo1";
+//const char * fifo2 = "./fifo2";
 // Creating the named file(FIFO)
-int f1 = mkfifo(fifo1,S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH);
-int f2 = mkfifo(fifo2,S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH);
-
+#define FILE1 "fifo1"
+#define FILE2 "fifo2"
+#define NMAX 100
 // Structs
 
 struct request
@@ -20,12 +19,9 @@ struct request
 	int id; 
 	int n;
 };
-typdef struct request request; 
-
 struct response
 {
 	int idc;
 	int ids;
-	int * t ;
+	int t[];
 };
-typdef struct response response; 
