4c56e4b327ec92df35d89dd501113ee7bc8cf496
Chahed Ons <58376907+ChahedOns@users.noreply.github.com>
2022-11-10 23:06:42 +0100

    Update Serveur.c

--- Serveur.c
+++ Serveur.c
@@ -5,6 +5,8 @@
 void hand_wakeUpClt(int sig){
 
 	printf("Bonne réception de la réponse!\n");
+	
+	
 }
  
 void hand_endServ(int sig){
@@ -37,22 +39,24 @@ f1 = open(FILE1,O_RDONLY);
 f2 = open(FILE2 ,O_WRONLY);
 if ( f1 == -1 || f2 == -1)
 {
-printf (" Ouverture des tubes impossible ");
+printf (" Ouverture des tubes impossible \n");
 exit(-1);
 };
-printf(" valeur de f1 = %d \n valeur de f2 = %d ", f1,f2 );
+printf(" valeur de f1 = %d \n valeur de f2 = %d \n ", f1,f2 );
 /* Installation des Handlers */
 	
 	signal(SIGINT,hand_endServ);
 	signal(SIGUSR1,hand_wakeUpClt);
 while(1){
-	printf("im here!");
+	printf("im here!\n");
 	/* lecture d’une question */
 		 read (f1 ,&rq , sizeof ( struct request ));
-	/* construction de la réponse */
+		 printf("le nbr n = %d", rq.n);
+		 /* construction de la réponse */
 		 int i;
-		for (i=0 ; i<rq.n ; i++){
-			rs.t[i] = rand()%NMAX;
+		 for (i = 0; i < rq.n; i++)
+		 {
+			 rs.t[i] = rand();
 		}
 		rs.idc = rq.id;
 		rs.ids = getpid();
@@ -61,7 +65,5 @@ while(1){
 	/* envoi du signal SIGUSR1 au client concerné */
 		kill(rs.idc,SIGUSR1);
 }
-close(f1);
-close(f2);
 
 }
