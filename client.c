f67ff4823d03b9b60e2a22f7806c9b5f602c1c51
Chahed Ons <58376907+ChahedOns@users.noreply.github.com>
2022-11-10 22:41:36 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,12 +1,11 @@
-#include "serv_cli_fifo.h"
-#include "Handlers_Cli.h"
-request r;
-response * s;
-
+#include "ser_cli_fifo.h"
+#include "handlers.h"
+struct request r;
+struct response s;
+int f1,f2;
 void hand_wakeUpClt(int sig){
-
-//nlawjo 3la l réponse mtee3i fel fifo 2 ! 
-read(f2,s, 1);
+printf("Client is up!");
+printf("\n" );
 
 }
 
@@ -16,20 +15,26 @@ main(){
 int x;
 /* Création des tubes nommés */
 
-if ( mkfifo(fifo1,S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH) == -1 ){
+/*if ( mkfifo(F1,0666) == -1 ){
 	perror("Error lors de la création du tube 1 !");
 }
-if ( mkfifo(fifo2,S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH) == -1 ){
+if ( mkfifo(F2,0666) == -1 ){
 	perror("Error lors de la création du tube 2 !");
-}
+}*/
 
 /* Ouverture des tubes nommés */
 
-	f1 = open(fifo1 , O_WRONLY);
-	f2 = open(fifo2, O_RDONLY);
+	f1 = open(FILE1,O_WRONLY);
+	f2 = open(FILE2,O_RDONLY);
+	if ( f1 == -1 || f2 == -1)
+	{
+	printf (" Ouverture des tubes impossible ");
+	exit(-1);
+	};
+	printf(" valeur de f1 = %d \n valeur de f2 = %d ", f1,f2 );
 
 /* Installation des Handlers */
-	signal(SIGUSR1, hand_wakeUpClt());
+	signal(SIGUSR1, hand_wakeUpClt);
 
 /* Construction et envoi d’une question */
 		printf("Saisie un entier\n");
@@ -38,17 +43,18 @@ if ( mkfifo(fifo2,S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH) == 
 		r.id= getpid();
 		r.n=x;
 		//envoi
-		write(f1,r);
+		write(f1,&r , sizeof(struct request ));
 /* Attente de la réponse */
 		pause();
 /* Lecture de la réponse */
-		read(f2,s,1);
+		read(f2,&s , sizeof (struct response ));
 /* Envoi du signal SIGUSR1 au serveur */
 		kill (s.ids,SIGUSR1);
 /* Traitement local de la réponse */
-		int i=0;
-		printf("La réponse du Serveur est : ")
-		for (int i=0 ; i<x;i++){
+		
+		printf("La réponse du Serveur est : ");
+		int i;
+		for (i=0; i<x;i++){
 			printf("%d ", s.t[i]);
 		}
 	close(f1);
